aadce40f2b22ad8c1e3e2bf4786e7cb0e26bf69c
mouse_484 <escmouse484@gmail.com>
2023-01-04 14:00:23 +0000

    feat: default eslint rule
    test passed

---
+++ packages/test/src/eslint/default/a.test.js
@@ -0,0 +1,18 @@
+// eslint-disable-next-line no-undef
+a;
+
+const hello1 = 'world';
+
+console.log(hello1);
+
+/* eslint-disable no-unused-vars */
+
+// eslint-disable-next-line no-var
+var hello2 = 'world';
+
+// eslint-disable-next-line prefer-const
+let hello3 = 'world';
+
+let number = 1;
+
+number++;
