e1cdada214485585fec180186d199f2e3a6986f2
mouse_484 <escmouse484@gmail.com>
2023-01-04 06:35:53 +0000

    feat(eslint): add default import

--- packages/eslint/default/index.js
+++ packages/eslint/default/index.js
@@ -3,8 +3,11 @@ module.exports = {
   extends: [
     '@mouse_484/eslint-config-base',
     '@mouse_484/eslint-config-typescript',
+    '@mouse_484/eslint-config-import',
+    'plugin:import/typescript',
     '@mouse_484/eslint-config-prettier',
   ],
+  settings: { 'import/resolver': { typescript: true, node: true } },
   env: {
     browser: true,
     node: true,
