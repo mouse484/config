817c16910d179b25d364073bf23ab89115604eea
mouse_484 <escmouse484@gmail.com>
2023-01-04 13:48:34 +0000

    feat: default eslint rule
    test passed

--- packages/eslint/default/index.js
+++ packages/eslint/default/index.js
@@ -1,15 +1,16 @@
+/* eslint-disable no-undef */
 /** @type {import('eslint/lib/shared/types').ConfigData} */
 module.exports = {
   extends: [
-    '@mouse_484/eslint-config-base',
-    '@mouse_484/eslint-config-typescript',
-    '@mouse_484/eslint-config-import',
-    'plugin:import/typescript',
-    '@mouse_484/eslint-config-prettier',
+    './.eslintrc.yml',
+    //   '@mouse_484/eslint-config-typescript',
+    //   '@mouse_484/eslint-config-import',
+    //   'plugin:import/typescript',
+    //   '@mouse_484/eslint-config-prettier',
   ],
-  settings: { 'import/resolver': { typescript: true, node: true } },
+  // settings: { 'import/resolver': { typescript: true, node: true } },
   env: {
-    browser: true,
-    node: true,
+    // browser: true,w
+    // node: true,
   },
 };
