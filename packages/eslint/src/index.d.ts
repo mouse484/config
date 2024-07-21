819a481e0f39275a2af501970d84bc2bc6533b13
mouse_484 <38714187+mouse484@users.noreply.github.com>
2024-07-21 02:11:55 +0000

    fix: eslint config typed

---
+++ packages/eslint/src/index.d.ts
@@ -0,0 +1,8 @@
+import { Awaitable, OptionsConfig, TypedFlatConfigItem } from "@antfu/eslint-config"
+
+declare function mouse(
+    options: OptionsConfig & TypedFlatConfigItem,
+    ...userConfigs: Awaitable<TypedFlatConfigItem | TypedFlatConfigItem[]>[]
+): Promise<TypedFlatConfigItem>
+
+export { mouse }
