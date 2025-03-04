1924c60234943a207e3f6653cb3c4e33776300dd
mouse484 <38714187+mouse484@users.noreply.github.com>
2025-03-04 22:57:22 +0900

    feat(lint): TypeScript用カスタム設定の無効化

--- packages/eslint/src/index.js
+++ packages/eslint/src/index.js
@@ -2,11 +2,6 @@
 import antfu, {
   GLOB_ASTRO,
   GLOB_SRC,
-  GLOB_TS,
-  GLOB_TSX,
-  interopDefault,
-  renameRules,
-  resolveSubOptions,
 } from '@antfu/eslint-config'
 
 /** @type {import('@antfu/eslint-config')["antfu"]} */
@@ -33,40 +28,6 @@ async function mouse(options, ...userConfigs) {
       ],
     },
   })
-
-  if (options?.typescript) {
-    const pluginTs = await interopDefault(
-      import('@typescript-eslint/eslint-plugin'),
-    )
-
-    /**
-     * @param {string} key
-     * @returns {import("eslint").Linter.RulesRecord}
-     * if tsconfigPath is defined, append '-type-checked' to the key
-     */
-    const getRules = (key) => {
-      const typescriptOptions = resolveSubOptions(options, 'typescript')
-      const tsconfigPath
-        = 'tsconfigPath' in typescriptOptions
-          ? typescriptOptions.tsconfigPath
-          : undefined
-
-      return renameRules(
-        pluginTs.configs[tsconfigPath ? `${key}-type-checked` : key]?.rules
-        ?? {},
-        { '@typescript-eslint': 'ts' },
-      )
-    }
-
-    configs.push({
-      name: 'mouse/typescript',
-      files: [GLOB_TS, GLOB_TSX],
-      rules: {
-        ...getRules('strict'),
-        ...getRules('stylistic'),
-      },
-    })
-  }
 
   if (options?.stylistic) {
     configs.push({
