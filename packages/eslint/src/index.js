056a359b4da2dc6b2cddd226f2fc1c68e10a0ba3
mouse484 <38714187+mouse484@users.noreply.github.com>
2025-11-20 23:04:49 +0900

    feat(eslint): enhance formatter options with package checks and defaults

--- packages/eslint/src/index.js
+++ packages/eslint/src/index.js
@@ -1,4 +1,5 @@
-import antfu, { GLOB_JS, GLOB_JSX } from '@antfu/eslint-config'
+import antfu, { GLOB_JS, GLOB_JSX, isPackageInScope } from '@antfu/eslint-config'
+import { isPackageExists } from 'local-pkg'
 import astro from './configs/astro.js'
 import base from './configs/base.js'
 import perfectionist from './configs/perfectionist.js'
@@ -16,6 +17,30 @@ async function mouse(options, ...userConfigs) {
     },
     stylistic: true,
     ...options,
+  }
+
+  if (options.formatters === true || typeof options.formatters === 'object') {
+    options.formatters = typeof options.formatters === 'boolean' ? {} : options.formatters
+
+    /**
+     * @see https://github.com/antfu/eslint-config/blob/f6d1a566fbe3d76a9fdc12dcba87302b5f0c9301/src/configs/formatters.ts#L30-L39
+     */
+    const isPrettierPluginXmlInScope = isPackageInScope('@prettier/plugin-xml')
+    options.formatters = {
+      astro: isPackageInScope('prettier-plugin-astro'),
+      css: true,
+      graphql: true,
+      html: true,
+      markdown: true,
+      slidev: isPackageExists('@slidev/cli'),
+      svg: isPrettierPluginXmlInScope,
+      xml: isPrettierPluginXmlInScope,
+      ...options.formatters,
+    }
+    options.formatters.prettierOptions = {
+      printWidth: 100,
+      ...options.formatters.prettierOptions,
+    }
   }
 
   const configs = [
