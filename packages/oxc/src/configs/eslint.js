e20375ca3b9855eb281397c9e04245f33d817e1f
mouse <me@mousedev.page>
2026-08-01 09:06:52 +0900

    feat(oxc): update ESLint rules to prefer perfectionist plugin for sorting

--- packages/oxc/src/configs/eslint.js
+++ packages/oxc/src/configs/eslint.js
@@ -4,6 +4,10 @@ export default defineConfig({
   rules: {
     'eslint/id-length': ['off'],
     'eslint/func-style': ['error', 'declaration', { allowArrowFunctions: true }],
+    /**
+     * Prefer `perfectionist` plugin for sorting
+     */
     'eslint/sort-imports': ['off'],
+    'eslint/sort-keys': ['off'],
   },
 })
