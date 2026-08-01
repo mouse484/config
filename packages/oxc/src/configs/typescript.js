dc6801373aa82eb211eb0f201dce3e79579b7323
mouse <me@mousedev.page>
2026-08-01 09:04:55 +0900

    feat(oxc): disable explicit-module-boundary-types rule in TypeScript configuration

--- packages/oxc/src/configs/typescript.js
+++ packages/oxc/src/configs/typescript.js
@@ -3,5 +3,6 @@ import { defineConfig } from 'oxlint'
 export default defineConfig({
   rules: {
     'typescript/explicit-function-return-type': ['off'],
+    'typescript/explicit-module-boundary-types': ['off'],
   },
 })
